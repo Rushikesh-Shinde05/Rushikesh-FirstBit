4c345f473a9c2fc5815959d1442e1e4e532a6427
Rushikesh <rushikeshshinde58075@gmail.com>
2025-06-10 18:56:07 +0530

    assignment updated

---
+++ C programming/assignment/assignment 1/even or odd.c
@@ -0,0 +1,15 @@
+//Q1.Write a program to check whether a number is even or odd. 
+
+# include<stdio.h>
+void main()
+{
+	int a;
+	printf("enter the number :");
+	scanf("%d",&a);
+	if(a%2 ==0)
+	printf("this number is even %d",a);
+	else 
+	printf("this number is odd %d",a);
+	
+	
+}
---
+++ C programming/assignment/assignment 1/vowel or consonant.c
@@ -0,0 +1,11 @@
+//Q4.Write a program to check whether a given character is a vowel or consonant.
+
+# include<stdio.h>
+void main ()
+{
+	char ch = 'e';
+	if(ch == 'a' || ch =='e' || ch =='i' || ch == 'o' || ch == 'u')
+	printf("this charecter is is vowels");
+	else
+	printf("this charecter is consonent");
+ } 
